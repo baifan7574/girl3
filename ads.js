021c7a23116fe6661a9d0242595f572d77e105b6
baifan7574 <baifan7574@gmail.com>
2025-07-27 02:21:02 +0800

    One, two.

---
+++ ads.js
@@ -0,0 +1,20 @@
+
+// Monetag - In-Page Push (Anti-AdBlock) - Zone ID: 9629285
+(function() {
+  var push = document.createElement('script');
+  push.src = 'https://fpyf8.com/88/tag.min.js';
+  push.async = true;
+  push.setAttribute('data-zone', '9629285');
+  push.setAttribute('data-cfasync', 'false');
+  document.head.appendChild(push);
+})();
+
+// Monetag - Vignette Banner (Anti-AdBlock) - Zone ID: 9629311
+(function() {
+  var vignette = document.createElement('script');
+  vignette.src = 'https://fpyf8.com/88/tag.min.js';
+  vignette.async = true;
+  vignette.setAttribute('data-zone', '9629311');
+  vignette.setAttribute('data-cfasync', 'false');
+  document.head.appendChild(vignette);
+})();
